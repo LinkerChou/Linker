68dd0e991c9da92b36de6478485a448f2205f7b7
lokavid <lokavid@outlook.com>
2015-06-07 21:43:16 +0800

    init

---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Lokavid on 15/6/7.
+//  Copyright (c) 2015年 Lokavid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
